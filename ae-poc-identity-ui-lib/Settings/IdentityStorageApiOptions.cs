52754980d1677c097795eb79f0b8f67c1455ebd9
Subkowski Igor <igor.subkowski@softaren.com>
2025-12-14 10:01:30 +0100

    Add project ae-poc-identity-ui-lib

---
+++ ae-poc-identity-ui-lib/Settings/IdentityStorageApiOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Ae.Poc.Identity.Ui.Settings;
+
+public sealed class IdentityStorageApiOptions
+{
+    public const string IdentityStorageApi = "IdentityStorageApi";
+
+    /// <summary>
+    /// z.B. "http://localhost:5005"
+    /// </summary>
+    public string ApiUrl { get; set; } = String.Empty;
+
+    /// <summary>
+    /// z.B. "/api/v1"
+    /// </summary>
+    public string ApiBasePath { get; set; } = String.Empty;
+}
