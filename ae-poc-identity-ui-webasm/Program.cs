928eb8108d01e8de1b1788b5ef804b278b0f9efb
Subkowski Igor <igor.subkowski@softaren.com>
2025-12-15 18:57:36 +0100

    Squashed commit of the following:     Improve contaner's files     Improve README.md     Improve for docker container. Remove Automapper.

--- ae-poc-identity-ui-webasm/Program.cs
+++ ae-poc-identity-ui-webasm/Program.cs
@@ -2,8 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Ae.Poc.Identity.Ui;
 using Ae.Poc.Identity.Ui.Extensions;
+using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Logging.AddSerilog();
 builder.Logging.AddConfiguration(
   builder.Configuration.GetSection("Logging"));
 
@@ -12,7 +19,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services
   .AddAppConfiguration(builder.Configuration)
-  .AddAppMapper()
   .AddAppServices();
 
 var app = builder.Build();
