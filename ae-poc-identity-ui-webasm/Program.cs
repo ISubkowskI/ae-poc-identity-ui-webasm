163038d3c342613335fe5d75b268e808ba01f29a
Subkowski Igor <igor.subkowski@softaren.com>
2025-12-15 08:33:15 +0100

    Add serilog

--- ae-poc-identity-ui-webasm/Program.cs
+++ ae-poc-identity-ui-webasm/Program.cs
@@ -2,8 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Ae.Poc.Identity.Ui;
 using Ae.Poc.Identity.Ui.Extensions;
+using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Logging.AddSerilog();
 builder.Logging.AddConfiguration(
   builder.Configuration.GetSection("Logging"));
 
